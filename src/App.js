554a74e45cde7855e20029461eddae0a73354a07
CodeSandbox <support@codesandbox.io>
2024-02-06 07:51:43 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import "./styles.css";
+import React, { useState } from "react";
 
-export default function App() {
+const tableData = [
+  { date: "2022-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-02", views: 150, article: "Article 2" },
+  { date: "2023-09-02", views: 120, article: "Article 3" },
+  { date: "2020-09-03", views: 200, article: "Article 4" },
+];
+
+const XTable = () => {
+  const [data, setData] = useState(tableData);
+
+  const sortByDate = () => {
+    const sortedData = [...data].sort((a, b) => {
+      const dateComparison = new Date(b.date) - new Date(a.date);
+      if (dateComparison !== 0) {
+        return dateComparison;
+      }
+      return b.views - a.views;
+    });
+    setData(sortedData);
+  };
+
+  const sortByViews = () => {
+    const sortedData = [...data].sort((a, b) => {
+      const viewsComparison = b.views - a.views;
+      if (viewsComparison !== 0) {
+        return viewsComparison;
+      }
+      return new Date(b.date) - new Date(a.date);
+    });
+    setData(sortedData);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>Date and Views Table</h1>
+      <button onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td>{row.date}</td>
+              <td>{row.views}</td>
+              <td>{row.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
+
+export default XTable;
